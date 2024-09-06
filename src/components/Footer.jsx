717eb8c34c90ef12fd1ee2c5d91352b5d7221d30
Manu <emmanuel.donahue@hotmail.fr>
2024-09-06 19:09:26 +0200

    end

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import phone from "../images/phone.svg";
+import portfolio from "../images/portfolio.svg";
+import github from "../images/github.svg";
 
 const Footer = () => {
 
     return <footer>
-        <h1>footer componant</h1>
+        <div className="contain-footer">
+            <div className="info-footer">
+                <div className="made-by">
+                    <h4>Design crée par Emmanuel DONAHUE avec la librairie <a href="https://fr.legacy.reactjs.org/" target="_blank" className="react">React</a>.</h4>
+                    <h4>Inspiré par le site de l'entreprise <a href="https://www.genieclimfrance.fr/" target="_blank" className="react">Génie Clim France</a>.</h4>
+                </div>
+                <div className="contact">
+                    <h4>Contact</h4>
+                    <div className="contain-contact">
+                        <div className="contain-icon-footer">
+                            <img src={phone} alt="logo téléphone" />
+                            <p>06 26 71 96 00</p>
+                        </div>
+                        <div className="contain-icon-footer">
+                            <img src={portfolio} alt="logo portfolio" />
+                            <p>emmanuel.donahue@hotmail.fr</p>                            
+                        </div>
+                        <a className="contain-icon-footer" href="https://github.com/Ednahe" target="_blank">
+                            <img src={github} alt="logo github" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </footer>
 }
 
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,74 +1,89 @@
 import React from "react";
-import corpo1 from '../images/corpo1.jpg';
-import corpo2 from '../images/corpo2.jpg';
-import corpo3 from '../images/corpo3.jpg';
-import corpo4 from '../images/corpo4.jpg';
-import corpo5 from '../images/corpo5.jpg';
-import arrowLeft from '../images/angle-left.svg';
-import arrowRight from '../images/angle-right.svg';
+import corpo1 from "../images/corpo1.jpg";
+import corpo2 from "../images/corpo2.jpg";
+import corpo3 from "../images/corpo3.jpg";
+import corpo4 from "../images/corpo4.jpg";
+import corpo5 from "../images/corpo5.jpg";
+import arrowLeft from "../images/angle-left.svg";
+import arrowRight from "../images/angle-right.svg";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const Slide = () => {
-
-    return <>
-    <div className="container-slide">
+  return (
+    <>
+      <div className="container-slide">
         <h1 className="title-slider">Voici le slider</h1>
-        <p className="p-slider"></p>
-        <Swiper 
-        effect={ "coverflow" } 
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={ 'auto' }
-        coverflowEffect={{
+        <p className="p-slider">
+          5 images défilable à l'infini, vous pouvez intéragir avec le slider
+          via les petites flèches en bas, via les ronds de pagination ou
+          simplement en faisant glisser les images.
+        </p>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
             rotate: 0,
             stretch: 0,
             depth: 100,
             modifier: 2.5,
-        }}
-        pagination={{ el:'.swiper-pagination', clickable:true, }}
-        navigation={{
-            nextEl:'.right',
-            prevEl:'.left',
+          }}
+          pagination={{ el: ".swiper-pagination", clickable: true }}
+          navigation={{
+            nextEl: ".right",
+            prevEl: ".left",
             clickable: true,
-
-        }}
-        modules={[ EffectCoverflow, Pagination, Navigation ]}
-        className="slider">
-            <SwiperSlide>
-                <img src={corpo1} alt="image de la slide" className="img-slide"/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={corpo2} alt="image de la slide" className="img-slide"/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={corpo3} alt="image de la slide" className="img-slide"/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={corpo4} alt="image de la slide" className="img-slide"/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={corpo5} alt="image de la slide" className="img-slide"/>
-            </SwiperSlide>
-            <div className="slider-controler">
-                <div className="left swiper-button">
-                    <img src={arrowLeft} className="arrow-slider" />
-                </div>
-                <div className="right swiper-button">
-                    <img src={arrowRight} className="arrow-slider" />
-                </div>
-                <div className="swiper-pagination"></div>
+          }}
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className="slider"
+        >
+          <SwiperSlide>
+            <img src={corpo1} alt="image de la slide" className="img-slide" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={corpo2} alt="image de la slide" className="img-slide" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={corpo3} alt="image de la slide" className="img-slide" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={corpo4} alt="image de la slide" className="img-slide" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={corpo5} alt="image de la slide" className="img-slide" />
+          </SwiperSlide>
+          <div className="slider-controler">
+            <div className="left swiper-button">
+              <img src={arrowLeft} className="arrow-slider" />
             </div>
+            <div className="right swiper-button">
+              <img src={arrowRight} className="arrow-slider" />
+            </div>
+            <div className="swiper-pagination"></div>
+          </div>
         </Swiper>
-    </div>
+        <p className="p-slider">
+          Pour observer le menu burger je vous invite à ouvrir l'inspecteur
+          (clic droit + inspecter) et à jouer avec la taille de l'écran en
+          selectionnant la barre latérale et en bougeant le curseur de gauche à
+          droite.
+        </p>
+        <p className="p-slider">
+          L'application est conçu pour être visible sur tous types de support à
+          partir d'un écran de 300px de large minimum, bon visionnage.
+        </p>
+      </div>
     </>
-}
+  );
+};
 
 export default Slide;
